70d60df8430c6b2064dc589d34814b7b9c89f8bd
public-jun <generalpublicjunjun@gmail.com>
2021-05-05 18:05:56 +0900

    update struct.h

--- includes/struct.h
+++ includes/struct.h
@@ -6,12 +6,15 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 22:02:22 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/05/02 15:18:24 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/05/05 18:01:42 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCT_H
 # define STRUCT_H
+
+# define ARGLIMIT 1001
+# define SORT_VALUE 0
 
 typedef struct s_num_list_node
 {
@@ -35,4 +38,31 @@ typedef struct s_list_group
 	t_num_list_node	*stack_b_test;
 	t_instr_list_node *head_instr;
 }	t_list_group;
+
+typedef struct s_pair
+{
+	int		value;
+	int		id;
+}	t_pair;
+
+typedef struct s_pivot
+{
+	int		p_value;
+	struct s_pivot	*next;
+	struct s_pivot	*prev;
+}	t_pivot;
+
+typedef struct s_info
+{
+	t_pair	n[ARGLIMIT + 10];
+	int		def[ARGLIMIT + 10];
+	t_pivot	*p_head;
+	int		all_size;
+	int		a_size;
+	int		b_size;
+	int		a_min;
+	int		b_min;
+	int		want;
+}	t_info;
+
 #endif
