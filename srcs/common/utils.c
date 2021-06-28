ad454e7597f6494126f451e35bbe2862292f4c9d
public-jun <generalpublicjunjun@gmail.com>
2021-06-28 13:15:34 +0900

    Revert "fix: ac == 1 && over INT print error"
    This reverts commit c083ba94ac7436e7867a232a1a44658f05943df1.
    
    コミットを戻す

--- srcs/common/utils.c
+++ srcs/common/utils.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 13:29:24 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/06/21 17:56:17 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/05/08 13:58:08 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,46 +22,23 @@ int	count_num_size(char **dst)
 	return (res);
 }
 
-static int	check_over_flow_in_ft_is_all_num(int sign, char *num)
-{
-	if (sign == -1)
-	{
-		if (check_int_overflow(sign, num + 1) == -1)
-			return (-1);
-	}
-	else
-	{
-		if (check_int_overflow(sign, num) == -1)
-			return (-1);
-	}
-	return (0);
-}
-
 int	ft_is_all_num(char *num)
 {
 	int		len;
 	int		i;
-	int		sign;
-	int		res;
 
 	i = 0;
-	sign = 1;
-	res = 0;
 	len = (int)ft_strlen(num);
 	if (len > 1 && num[i] == '0')
 		return (-1);
 	i = 0;
 	if (num[i] == '-')
-	{
 		i++;
-		sign = -1;
-	}
-	res = check_over_flow_in_ft_is_all_num(sign, num);
 	while (ft_isdigit(num[i]))
 		i++;
-	if (i == len && res != -1)
+	if (i == len)
 		return (0);
-	return (res);
+	return (-1);
 }
 
 int	check_int_overflow(int sign, const char *str)
